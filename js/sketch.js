b9406c0be1b0d62e495031796b2fd70be74bef45
Connor McCarthy <connormccarthy8@gmail.com>
2018-08-27 16:05:06 -0700

    add backgrounds for website and for game.

--- js/sketch.js
+++ js/sketch.js
@@ -2,8 +2,15 @@
 var players = [];
 var player;
 
+var backdrop;
+function preload() {
+  backdrop = loadImage('https://vignette.wikia.nocookie.net/avengersalliance/images/8/89/Combat_Background_048.jpg/revision/latest?cb=20130904191456&format=original');
+}
+
 function setup() {
   var canvas = createCanvas(1024, 576);
+  // image(backdrop, 0, 0);
+
   canvas.parent('gameBoard');
 
   player = new Player(0);
@@ -16,7 +23,7 @@ function setup() {
 
 //this function is called every frame, 30times a sec. Put things that need to be constantly updated in the draw() function
 function draw() {
-  background(93);
+  background(backdrop, 0,0);
 
   for(i=0;i<players.length;i++) {
     players[i].show();
