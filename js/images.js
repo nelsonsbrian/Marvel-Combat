5cf0904c8fd76b695b1c624c2f8a4f515db72ecf
Chris Crow <chrismcrow@gmail.com>
2018-08-28 00:23:20 -0700

    fix code typo

--- js/images.js
+++ js/images.js
@@ -24,13 +24,13 @@ function preload() {
 
   ironManSprite = new Sprites(loadImage('https://i.imgur.com/AqQ7brV.png'), loadImage('https://i.imgur.com/uZm54mX.png'), loadImage('https://i.imgur.com/U8EW5re.png'), loadImage('https://i.imgur.com/j7UjIdP.png'), loadImage('https://i.imgur.com/C6VfCFx.png'));
 
-  hulkSprite = new Sprite(loadImage('https://i.imgur.com/daAdlYF.png'), loadImage('https://i.imgur.com/Eg7ORuy.png'), loadImage('https://i.imgur.com/3o0afq3.png'), loadImage('https://i.imgur.com/jcarFX3.png'), loadImage('https://i.imgur.com/sXXs4Kv.png'));
+  hulkSprite = new Sprites(loadImage('https://i.imgur.com/daAdlYF.png'), loadImage('https://i.imgur.com/Eg7ORuy.png'), loadImage('https://i.imgur.com/3o0afq3.png'), loadImage('https://i.imgur.com/jcarFX3.png'), loadImage('https://i.imgur.com/sXXs4Kv.png'));
 
-  blackWidowSprite = new Sprite(loadImage('https://i.imgur.com/ggZosIJ.png'), loadImage('https://i.imgur.com/3RJZ2ms.png'), loadImage('https://i.imgur.com/xKu87V7.png'), loadImage('https://i.imgur.com/EQ0Mrvi.png'), loadImage('https://i.imgur.com/5UOHZWl.png'));
+  blackWidowSprite = new Sprites(loadImage('https://i.imgur.com/ggZosIJ.png'), loadImage('https://i.imgur.com/3RJZ2ms.png'), loadImage('https://i.imgur.com/xKu87V7.png'), loadImage('https://i.imgur.com/EQ0Mrvi.png'), loadImage('https://i.imgur.com/5UOHZWl.png'));
 
   captainAmericaSprite = new Sprites(loadImage('https://i.imgur.com/mk1C150.png'), loadImage('https://i.imgur.com/ZhNlTU1.png'), loadImage('https://i.imgur.com/0efMinz.png'), loadImage('https://i.imgur.com/SSmuGRh.png'), loadImage('https://i.imgur.com/N9misJ7.png'));
 
-  thorSprite = new Sprite(loadImage('https://i.imgur.com/Dd7gsqn.png'), loadImage('https://i.imgur.com/VGmDlkr.png'), loadImage('https://i.imgur.com/x34SXto.png'), loadImage('https://i.imgur.com/4LBlDdv.png'), loadImage('https://i.imgur.com/6PLFZq2.png'));
+  thorSprite = new Sprites(loadImage('https://i.imgur.com/Dd7gsqn.png'), loadImage('https://i.imgur.com/VGmDlkr.png'), loadImage('https://i.imgur.com/x34SXto.png'), loadImage('https://i.imgur.com/4LBlDdv.png'), loadImage('https://i.imgur.com/6PLFZq2.png'));
 
-  scarletWitchSprite = new Sprite(loadImage('https://i.imgur.com/HiTHeQu.png'), loadImage('https://i.imgur.com/U7md2BW.png'), loadImage('https://i.imgur.com/sifLeDp.png'), loadImage('https://i.imgur.com/zrHOnVN.png'), )
+  scarletWitchSprite = new Sprites(loadImage('https://i.imgur.com/HiTHeQu.png'), loadImage('https://i.imgur.com/U7md2BW.png'), loadImage('https://i.imgur.com/sifLeDp.png'), loadImage('https://i.imgur.com/zrHOnVN.png'), )
 }
