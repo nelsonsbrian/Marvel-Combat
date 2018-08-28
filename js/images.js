58aae7e9ad29e08484a5d110d4bf6943c0b3864a
Chris Crow <chrismcrow@gmail.com>
2018-08-28 00:19:19 -0700

    add sprites for Hulk, Thor, Black Widow, & Scarlet Witch.

--- js/images.js
+++ js/images.js
@@ -1,7 +1,15 @@
 var backdrop;
 var captainAmericaNeutral;
+
+//Left Side Characters
 var ironManSprite;
+var hulkSprite;
+var blackWidowSprite;
+
+//Right Side Characters
 var captainAmericaSprite;
+var thorSprite;
+var scarletWitchSprite;
 
 function Sprites(neutral, attack, block, special, range) {
   this.neutral = neutral;
@@ -16,5 +24,13 @@ function preload() {
 
   ironManSprite = new Sprites(loadImage('https://i.imgur.com/AqQ7brV.png'), loadImage('https://i.imgur.com/uZm54mX.png'), loadImage('https://i.imgur.com/U8EW5re.png'), loadImage('https://i.imgur.com/j7UjIdP.png'), loadImage('https://i.imgur.com/C6VfCFx.png'));
 
-  captainAmericaSprite = new Sprites(loadImage('https://i.imgur.com/mk1C150.png'), loadImage('https://i.imgur.com/ZhNlTU1.png'), loadImage('https://i.imgur.com/0efMinz.png'), loadImage('https://i.imgur.com/SSmuGRh.png'), loadImage('https://i.imgur.com/N9misJ7.png'))
+  hulkSprite = new Sprite(loadImage('https://i.imgur.com/daAdlYF.png'), loadImage('https://i.imgur.com/Eg7ORuy.png'), loadImage('https://i.imgur.com/3o0afq3.png'), loadImage('https://i.imgur.com/jcarFX3.png'), loadImage('https://i.imgur.com/sXXs4Kv.png'));
+
+  blackWidowSprite = new Sprite(loadImage('https://i.imgur.com/ggZosIJ.png'), loadImage('https://i.imgur.com/3RJZ2ms.png'), loadImage('https://i.imgur.com/xKu87V7.png'), loadImage('https://i.imgur.com/EQ0Mrvi.png'), loadImage('https://i.imgur.com/5UOHZWl.png'));
+
+  captainAmericaSprite = new Sprites(loadImage('https://i.imgur.com/mk1C150.png'), loadImage('https://i.imgur.com/ZhNlTU1.png'), loadImage('https://i.imgur.com/0efMinz.png'), loadImage('https://i.imgur.com/SSmuGRh.png'), loadImage('https://i.imgur.com/N9misJ7.png'));
+
+  thorSprite = new Sprite(loadImage('https://i.imgur.com/Dd7gsqn.png'), loadImage('https://i.imgur.com/VGmDlkr.png'), loadImage('https://i.imgur.com/x34SXto.png'), loadImage('https://i.imgur.com/4LBlDdv.png'), loadImage('https://i.imgur.com/6PLFZq2.png'));
+
+  scarletWitchSprite = new Sprite(loadImage('https://i.imgur.com/HiTHeQu.png'), loadImage('https://i.imgur.com/U7md2BW.png'), loadImage('https://i.imgur.com/sifLeDp.png'), loadImage('https://i.imgur.com/zrHOnVN.png'), )
 }
