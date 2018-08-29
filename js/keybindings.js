48269ee61cc2b98d464a1e0e31168d7acad319f1
Chris Crow <chrismcrow@gmail.com>
2018-08-29 14:57:17 -0700

    add style and radio buttons and form that starts game.

--- js/keybindings.js
+++ js/keybindings.js
@@ -1,3 +1,21 @@
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.keyCode] = true;
+        switch(e.keyCode){
+            case 37: case 39: case 38:  case 40: // Arrow keys
+            case 32: e.preventDefault(); break; // Space
+            default: break; // do not block other keys
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.keyCode] = false;
+    },
+false);
+
+
 //keybindings for both players
 function keyPressed() { //player1
   if (key === 'a') {
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
 // business logic
+var player1input = getRandomInt(0,4);
+var player2input = getRandomInt(6,10);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 // user logic
 $(document).ready(function() {
 
   $('#play').click(function(event) {
     event.preventDefault();
+    $('#selection').fadeIn();
     for (i = 0; i < heroes.length; i++) {
       $("#hero" + i + " h4").text(heroes[i][0]);
       $('#hero' + i).prepend("<img src='" + heroSprites[i].image + "'>");
     };
+    $('#play').hide();
   });
+
+  $('.left').click(function() {
+    $(this).parents().find('.left').removeClass('selected');
+    $(this).addClass('selected');
+    player1input = $(this).attr('value');
+  });
+
+  $('.right').click(function() {
+    $(this).parents().find('.right').removeClass('selected');
+    $(this).addClass('selected');
+    player2input = $(this).attr('value');
+  });
+
+  $('#start').click(function(event) {
+    event.preventDefault();
+    $('#selection').hide();
+    $('#gamePlay').fadeIn();
+    console.log("works");
+    startGame();
+  });
+
 
   $('#reset').click(function() {
     $('#reset').blur();
+    document.location.reload();
     // gameReset();
   });
 });
