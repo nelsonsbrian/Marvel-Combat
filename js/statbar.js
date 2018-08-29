cdf6a20dcfae357d573d872baaed8695cdbafbc5
Guest User <Guest@epicodus-4.local>
2018-08-29 10:47:55 -0700

    add styling to the gameboard

--- js/statbar.js
+++ js/statbar.js
@@ -4,7 +4,7 @@ function StatBar(name, health, maxHealth, power, powerMax) {
   this.pMaxHealth = maxHealth;
   this.pPower = power;
   this.pPowerMax = powerMax;
-  this.healthSize = [400,50];
+  this.healthSize = [350,40];
   this.powerSize = [400,30];
   this.pHealthPercent = this.pHealth / this.pMaxHealth;
   this.pPowerPercent = this.pPower / this.pPowerMax;
@@ -23,23 +23,23 @@ function StatBar(name, health, maxHealth, power, powerMax) {
     this.pPowerPercent = power / this.pPowerMax;
     if (0 === player.indexNum) {
       //player 1 health
-      fill(255,0,0);
-      rect(0,0,this.healthSize[0],this.healthSize[1]);
-      fill(0,255,0);
+      fill(188, 26, 39);
+      rect(10,10,this.healthSize[0],this.healthSize[1], 20, 15, 10, 5);
+      fill(110, 196, 76);
       let greenHealth = this.pHealthPercent * this.healthSize[0];
       greenHealth = constrain(greenHealth, 0, this.healthSize[0]);
-      rect(0,0,greenHealth,this.healthSize[1]);
-      fill(255);
+      rect(10,10,greenHealth,this.healthSize[1], 20, 15, 10, 5);
+      fill(0, 0, 0);
       textSize(30);
-      textStyle(BOLD);
+      textStyle(NORMAL);
       text(name, 50, 40);
     //player 2 power
       fill(0);
-      rect(0,height-this.powerSize[1],this.powerSize[0],this.powerSize[1]);
+      rect(0,height-this.powerSize[1],this.powerSize[0],this.powerSize[1], 0, 15, 0, 5);
       fill(255,255,0);
       let bluePower = this.pPowerPercent * this.powerSize[0];
       bluePower = constrain(bluePower, 0, this.powerSize[0]);
-      rect(0,height-this.powerSize[1],bluePower,this.powerSize[1]);
+      rect(0,height-this.powerSize[1],bluePower,this.powerSize[1], 0, 15, 0, 5);
       fill(255);
       textSize(30);
       textStyle(BOLD);
@@ -48,23 +48,23 @@ function StatBar(name, health, maxHealth, power, powerMax) {
   if (1 === player.indexNum) {
     //hulkSprite,Captain America
       //player2 health
-      fill(255,0,0);
-      rect(width - this.healthSize[0],0,width,this.healthSize[1]);
-      fill(0,255,0);
+      fill(188, 26, 39);
+      rect(width - 10 - this.healthSize[0],10,this.healthSize[0],this.healthSize[1], 15, 20, 5, 10);
+      fill(110, 196, 76);
       let greenHealth = this.pHealthPercent * this.healthSize[0];
       greenHealth = constrain(greenHealth, 0, this.healthSize[0]);
-      rect(width - this.healthSize[0],0,greenHealth,this.healthSize[1]);
-      fill(255);
+      rect(width - 10 - (greenHealth),10,width - 10 -(width - 10 - (greenHealth)),this.healthSize[1], 15, 20, 5, 10);
+      fill(0, 0, 0);
       textSize(30);
-      textStyle(BOLD);
+      textStyle(NORMAL);
       text(name, width - this.healthSize[0] + 10, 40);
       //player 2 power
       fill(0);
-      rect(width - this.powerSize[0],height-this.powerSize[1],this.powerSize[0],this.powerSize[1]);
+      rect(width - this.powerSize[0],height-this.powerSize[1],this.powerSize[0],this.powerSize[1], 15, 00, 0, 0);
       fill(255,255,0);
       let bluePower = this.pPowerPercent * this.powerSize[0];
       bluePower = constrain(bluePower, 0, this.powerSize[0]);
-      rect(width - this.powerSize[0],height-this.powerSize[1],bluePower,this.powerSize[1]);
+      rect(width - bluePower, height - this.powerSize[1],height-this.powerSize[1] ,this.powerSize[1], 15, 20, 10, 5);
       fill(255);
       textSize(30);
       textStyle(BOLD);
